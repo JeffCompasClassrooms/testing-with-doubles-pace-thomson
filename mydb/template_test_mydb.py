ac7006d1724d51673b550aa0dec887d33b104c2e
Pace Thomson <pacethomson@gmail.com>
2025-09-26 18:30:21 -0600

    got mydb tests done

--- mydb/template_test_mydb.py
+++ mydb/template_test_mydb.py
@@ -45,15 +45,52 @@ def describe_MyDB():
             mock_dump.assert_not_called()
     
     def describe_loadStrings():
-        @todo
         def it_loads_an_array_from_a_file_and_returns_it(mocker):
-            pass
+            # set up stubs & mocks first
+            mocker.patch("os.path.isfile", return_value=True)
+            mock_open = mocker.patch("builtins.open", mocker.mock_open())
+            mock_load = mocker.patch("pickle.load", return_value=["one", "two"])
+            db = MyDB("mydatabase.db")
+
+            # execute on the test subject
+            result = db.loadStrings()
+
+            print('result', result)
+
+            # assert what happened
+            mock_open.assert_called_once_with("mydatabase.db", "rb")
+            mock_load.assert_called_once_with(mock_open.return_value)
+            assert result == ["one", "two"]
+
+            
+
     def describe_saveStrings():
-        @todo
         def it_saves_the_given_array_to_a_file(mocker):
-            pass
+            # set up stubs & mocks first
+            mocker.patch("os.path.isfile", return_value=True)
+            mock_open = mocker.patch("builtins.open", mocker.mock_open())
+            mock_dump = mocker.patch("pickle.dump")
+            db = MyDB("mydatabase.db")
+
+            # execute on the test subject
+            test_data = ["hello", "world"]
+            db.saveStrings(test_data)
+
+            # assert what happened
+            mock_open.assert_called_once_with("mydatabase.db", "wb")
+            mock_dump.assert_called_once_with(test_data, mock_open.return_value)
     
     def describe_saveString():
-        @todo
         def it_appends_string_element_to_existing_database(mocker):
-            pass
+            # set up stubs & mocks first
+            mocker.patch("os.path.isfile", return_value=True) # so __init__ is a no-op
+            db = MyDB("mydatabase.db")
+            mock_load = mocker.patch.object(db, "loadStrings", return_value=["existing string"])
+            mock_save = mocker.patch.object(db, "saveStrings")
+
+            # execute on the test subject
+            db.saveString("new string")
+
+            # assert what happened
+            mock_load.assert_called_once_with()
+            mock_save.assert_called_once_with(["existing string", "new string"])
